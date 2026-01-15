37f56dc71c2636b2f11eeab5c77e0d69845dceb2
Ankit <knjangir24@gmail.com>
2026-01-15 17:57:22 +0530

    Update SMTP password configuration

--- src/core/config.py
+++ src/core/config.py
@@ -10,7 +10,7 @@ class Settings(BaseSettings):
     SMTP_HOST: str = "smtp.gmail.com"
     SMTP_PORT: int = 587
     SMTP_USER: str = "skeymouseit@gmail.com" # Add in .env
-    SMTP_PASSWORD: str = "ucdo pfiu lqpg penz" # Add in .env
+    SMTP_PASSWORD: str = "ucdopfiulqpgpenz" # Add in .env
 
     # JWT Configuration
     SECRET_KEY: str = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7" # Change in production!
